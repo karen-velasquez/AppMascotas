a9284e21d8b0dd939707d6daf4b49b056a331721
karen-velasquez <maritza.karen.flores.velasquez@gmail.com>
2021-04-30 10:05:11 -0400

    Generando el diseño de la adicion de datos

---
+++ app/src/main/java/com/example/mascotasproject/AddData.java
@@ -0,0 +1,14 @@
+package com.example.mascotasproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_data);
+    }
+}
--- app/src/main/java/com/example/mascotasproject/FirebaseCard.java
+++ app/src/main/java/com/example/mascotasproject/FirebaseCard.java
@@ -3,6 +3,7 @@ package com.example.mascotasproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 public class FirebaseCard extends AppCompatActivity {
@@ -13,5 +14,17 @@ public class FirebaseCard extends AppCompatActivity {
         setContentView(R.layout.firebase_card);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportFragmentManager().beginTransaction().replace(R.id.wrapper,new recfragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id=item.getItemId();
+
+        //acciones al hacer click en el action bar
+        if(id ==R.id.action_add){
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

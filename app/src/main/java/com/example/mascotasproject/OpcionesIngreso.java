12eb00fadaee067c056bd17954ff4fc7e3aa444a
karen-velasquez <maritza.karen.flores.velasquez@gmail.com>
2021-05-13 01:54:37 -0400

    localizaciones desde firebase, obtencion fecha y hora , completo

--- app/src/main/java/com/example/mascotasproject/OpcionesIngreso.java
+++ app/src/main/java/com/example/mascotasproject/OpcionesIngreso.java
@@ -5,7 +5,9 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.mascotasproject.IA.CameraActivity;
 import com.example.mascotasproject.IA.Classifier;
@@ -20,14 +22,14 @@ import java.util.Date;
 
 public class OpcionesIngreso extends AppCompatActivity {
 
-    Button musuario,minvitado,mnuevo;
+    ImageButton musuario,minvitado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones_ingreso);
         musuario=findViewById(R.id.usuario);
         minvitado=findViewById(R.id.invitado);
-        mnuevo=findViewById(R.id.nuevo);
+
 
         musuario.setOnClickListener(v-> {
             Intent intent=new Intent(OpcionesIngreso.this, ClassifierActivity.class);
